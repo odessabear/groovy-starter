8c02241d1cd1c77846e5cf0089a69b719d6c5cc1
Oleksandr Mykhailov <bearodessa@gmail.com>
2022-09-07 11:33:22 +0300

    test commit

--- src/main/groovy/com/odebar/OperatorsRunner.groovy
+++ src/main/groovy/com/odebar/OperatorsRunner.groovy
@@ -8,8 +8,8 @@ assert 3.intdiv(2) == 1
 assert 10 % 3 == 1
 assert 2**3 == 8
 
-//def result = (int) (3 / 2)
-def result = (3 / 2) as Integer
+def result = (int) (3 / 2)
+def result2 = (3 / 2) as Integer
 
 println(result <= 9)
 
